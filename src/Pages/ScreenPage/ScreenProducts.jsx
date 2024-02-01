9ced17e57b722b94fec0f41472dd6248d6448e36
thanhdoan1201 <nokageed9@gmail.com>
2024-02-01 22:17:13 +0700

    Update UI Screen page

--- src/Pages/ScreenPage/ScreenProducts.jsx
+++ src/Pages/ScreenPage/ScreenProducts.jsx
@@ -1,9 +1,74 @@
 const ScreenProducts = () => {
     return (
-        <div className="container row">
-
+        <div class="container">
+            <div className="row">
+                <div className="col-6 p-2 mt-2 col-sm-6 col-xl-3 col-md-4 col-lg-3">
+                    <div className="rounded-4 bg-light text-center">
+                        <div className="btn-soSanh">
+                            {/* placehoder */}
+                        </div>
+                        <div className="img">
+                            <img src="https://macone.vn/wp-content/uploads/2023/11/Macbook-14-inch-Silver-300x250.png" alt="" />
+                        </div>
+                        <div className="nameProduct">
+                            <a href="#">Sản Phẩm 1</a>
+                        </div>
+                        <div className="price p-2">
+                            <h4 class="fw-bold fs-5">60.000 VNĐ</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6 p-2 mt-2 col-sm-6 col-xl-3 col-md-4 col-lg-3">
+                    <div className="rounded-4 bg-light text-center">
+                        <div className="btn-soSanh">
+                            {/* placehoder */}
+                        </div>
+                        <div className="img">
+                            <img src="https://macone.vn/wp-content/uploads/2023/11/Macbook-14-inch-Silver-300x250.png" alt="" />
+                        </div>
+                        <div className="nameProduct">
+                            <a href="#">Sản Phẩm 2</a>
+                        </div>
+                        <div className="price p-2">
+                            <h4 class="fw-bold fs-5">60.000 VNĐ</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6 p-2 mt-2 col-sm-6 col-xl-3 col-md-4 col-lg-3">
+                    <div className="rounded-4 bg-light text-center">
+                        <div className="btn-soSanh">
+                            {/* placehoder */}
+                        </div>
+                        <div className="img">
+                            <img src="https://macone.vn/wp-content/uploads/2023/11/Macbook-14-inch-Silver-300x250.png" alt="" />
+                        </div>
+                        <div className="nameProduct">
+                            <a href="#">Sản Phẩm 3</a>
+                        </div>
+                        <div className="price p-2">
+                            <h4 class="fw-bold text-decoration-line-through fs-5">60.000 VNĐ</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6 p-2 mt-2 col-sm-6 col-xl-3 col-md-4 col-lg-3">
+                    <div className="rounded-4 bg-light text-center">
+                        <div className="btn-soSanh">
+                            {/* placehoder */}
+                        </div>
+                        <div className="img">
+                            <img src="https://macone.vn/wp-content/uploads/2023/11/Macbook-14-inch-Silver-300x250.png" alt="" />
+                        </div>
+                        <div className="nameProduct">
+                            <a href="#">Sản Phẩm 4</a>
+                        </div>
+                        <div className="price p-2">
+                            <h4 class="fw-bold fs-5">60.000 VNĐ</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
-  };
-  
-  export default ScreenProducts;
+};
+
+export default ScreenProducts;
